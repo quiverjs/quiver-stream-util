781e72c4cce1b33139c41632221040831709c906
Soares Chen <soares.chen@gmail.com>
2014-09-12 20:55:03 +0800

    Pushback stream now have peek method.

--- es5/lib/pushback.js
+++ es5/lib/pushback.js
@@ -7,7 +7,8 @@ Object.defineProperties(exports, {
 });
 var $__quiver_45_promise__;
 var resolve = ($__quiver_45_promise__ = require("quiver-promise"), $__quiver_45_promise__ && $__quiver_45_promise__.__esModule && $__quiver_45_promise__ || {default: $__quiver_45_promise__}).resolve;
-var pushbackStream = (function(readStream, buffers) {
+var pushbackStream = (function(readStream) {
+  var buffers = arguments[1] !== (void 0) ? arguments[1] : [];
   if (readStream.pushback) {
     readStream.pushback(buffers);
     return readStream;
@@ -22,10 +23,23 @@ var pushbackStream = (function(readStream, buffers) {
   });
   newReadStream.closeRead = (function(err) {
     buffers = [];
-    return readStream.CloseRead(err);
+    return readStream.closeRead(err);
   });
   newReadStream.pushback = (function(moreBuffers) {
     buffers = moreBuffers.concat(buffers);
+  });
+  newReadStream.peek = (function() {
+    if (buffers[0])
+      return resolve({data: buffers[0]});
+    return readStream.read((function($__1) {
+      var $__2 = $__1,
+          data = $__2.data,
+          closed = $__2.closed;
+      if (closed)
+        return {closed: closed};
+      buffers.push(data);
+      return {data: data};
+    }));
   });
   return newReadStream;
 });
